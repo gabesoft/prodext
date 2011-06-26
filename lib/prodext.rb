77721f280554210f9bf967c07ac121209922c287
gabriel <gabesoft@gmail.com>
2011-06-25 18:23:10 -0700

    fetch page

--- lib/prodext.rb
+++ lib/prodext.rb
@@ -1,5 +1,18 @@
-require "prodext/version"
+require 'prodext/version'
+require 'net/https'
+
 
 module Prodext
-  # Your code goes here...
+  def self.fetch_page url
+    uri = URI.parse url
+    response = get_response uri
+    response.body
+  end
+
+  def self.get_response uri
+    http = Net::HTTP.new uri.host, uri.port
+    req = Net::HTTP::Get.new uri.path.empty? ? '/' : uri.path
+    res = http.start.request req
+    res
+  end
 end
--- spec/prodext_spec.rb
+++ spec/prodext_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Prodext do
-  pending 'implement'
+  describe "fetch_page" do
+    before :each do
+      #TODO: add before ops here
+    end
+
+    it "should return the html of a given page url" do
+      html = Prodext.fetch_page "http://www.google.com"
+      html.should include "html"
+    end
+  end
 end
