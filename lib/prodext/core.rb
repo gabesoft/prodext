ebeda4daf28c1cff07b967848505393a24eff33e
gabriel <gabesoft@gmail.com>
2011-06-27 19:45:14 -0700

    post requests

--- lib/prodext/core.rb
+++ lib/prodext/core.rb
@@ -15,23 +15,52 @@ module Prodext
     end
   end
 
-  def self.fetch_page(url, cookies_path = nil)
+  def self.page_get(url, cookies_path = nil)
     uri = URI.parse url
     cookies = CookieStoreSerializer.file_load cookies_path
-    response = get_response uri, cookies
-    body = response.body
+    response = http_get uri, cookies
+
     cookies.merge! response['Set-Cookie']
     CookieStoreSerializer.file_save cookies_path, cookies
-    body
+
+    response.body
   end
 
-  def self.get_response(uri, cookies)
+  def self.page_post(url, cookies_path = nil, params)
+    uri = URI.parse url
+    cookies = CookieStoreSerializer.file_load cookies_path
+    response = http_post uri, cookies, params
+    
+    cookies.merge! response['Set-Cookie']
+    CookieStoreSerializer.file_save cookies_path, cookies
+
+    response.body
+  end
+
+  def self.http_get(uri, cookies)
     http = Net::HTTP.new uri.host, uri.port
     opts = {}
     opts['Cookie'] = cookies.to_s unless cookies.empty?
 
     request = Net::HTTP::Get.new uri.request_uri, opts
     response = http.start.request request
+    response
+  end
+
+  def self.http_post(uri, cookies, params)
+    http = Net::HTTP.new uri.host, uri.port
+    if uri.scheme == 'https'
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    opts = {}
+    opts['Cookie'] = cookies.to_s unless cookies.empty?
+
+    request = Net::HTTP::Post.new uri.request_uri, opts
+    request.set_form_data params
+
+    response = http.request request
     response
   end
 
--- spec/prodext_spec.rb
+++ spec/prodext_spec.rb
@@ -1,14 +1,24 @@
 require 'spec_helper'
 
 describe Prodext do
-  describe "fetch_page" do
-    before :each do
-      #TODO: add before ops here
-    end
-
+  describe "page_get" do
     it "should return the html of a given page url" do
-      html = Prodext.fetch_page "http://www.google.com"
+      html = Prodext.page_get "http://www.google.com"
       html.should include "html"
     end
   end
+
+  describe "page_post" do
+    it "should generate a post request to a given url" do
+
+      url = "https://shop.safeway.com/register/registernew.asp?signin=1&returnTo=&register=&rzipcode=90028&zipcode=90028"
+      html = Prodext.page_post url, nil, {}
+
+      response = "<script language=\"javascript\">top.location = 'http://shop.safeway.com/superstore';</script>"
+      html.should eq response
+    end
+  end
+
+  # TODO: get page
+  # TODO: post page
 end
